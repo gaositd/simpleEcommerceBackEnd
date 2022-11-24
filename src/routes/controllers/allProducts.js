e173e60c60c9ccbe07ff14c18542722cf92e7ef4
Alvaro Gamez Chavez <gaositd@gmail.com>
2022-11-23 22:05:43 -0600

    ajustes al backend

--- src/routes/controllers/allProducts.js
+++ src/routes/controllers/allProducts.js
@@ -9,7 +9,7 @@ const allProducts = async (req, res) => {
     if (products.length) {
       res.status(200).json(products);
     } else {
-      res.status(404).json({ msg: `${NOT_FOUND}` });
+      res.status(200).json({ msg: `${NOT_FOUND}` });
     }
   } catch (err) {
     res.status(500).json({ msg: `${err.message} ${err.stack}` });
--- src/routes/controllers/getProductListController.js
+++ src/routes/controllers/getProductListController.js
@@ -1,39 +1,40 @@
 const { Op } = require("sequelize");
-const { product } = require('../../cxnMysql');
+const { product } = require("../../cxnMysql");
 const { NOT_FOUND } = process.env;
 
-const getProductList = async (req, res) =>{
+const getProductList = async (req, res) => {
   let search = req.params.search;
-  // search = search.toUpperCase();
-  try{
-    const data = await product.findAll({
-      whrere:{
-        name:{
-          [Op.like]: [`${search.toUpperCase()}`],
-        }
+
+  try {
+    // const data = await product.findAll({
+    //   whrere: {
+    //     name: {
+    //       [Op.like]: [`${search.toUpperCase()}`],
+    //     },
+    //   },
+    // });
+
+    const data = await product.findAll();
+
+    let productList = data.map((list) => {
+      list.name = list.name.toUpperCase();
+      return list;
+    });
+
+    productList = data.filter((list) => {
+      if (list.dataValues.name.includes(search.toUpperCase())) {
+        return list;
       }
     });
 
-    const productList = data.filter(list =>{
-      if(list.dataValues.name.includes(search.toUpperCase())){
-        return list;
-      };
-    });
-
-    if(productList.length){
-      res
-        .status(200)
-        .json(productList);
-    }else{
-      res
-        .status(404)
-        .json({msg: `${NOT_FOUND}`});
+    if (productList.length) {
+      res.status(200).json(productList);
+    } else {
+      res.status(200).json({ msg: `${NOT_FOUND}` });
     }
-  }catch(err){
-    res
-        .status(500)
-        .json({msg: `${err.message} ${err.stack}`});
+  } catch (err) {
+    res.status(500).json({ msg: `${err.message} ${err.stack}` });
   }
-}
+};
 
 module.exports = getProductList;
